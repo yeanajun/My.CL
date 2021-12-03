6dd0a5bf8b2659f9f304ed218c1414abf8a5b9dd
kwakchanhyuk <kwakchanhyuk@naver.com>
2021-12-03 17:27:56 +0900

    tag_data update(demo)

--- client/clients/models.py
+++ client/clients/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class Category_log(models.Model):
+    _id = models.ObjectIdField()
+    user_id = models.IntegerField(null=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+    grade = models.CharField(null=True, max_length=20)
+    subject = models.CharField(null=True, max_length=20)
+    achievement = models.CharField(null=True, max_length=20)
+    site = models.CharField(null=True, max_length=20)
+    jobdam = models.CharField(null=True, max_length=20)
+    pilgi = models.CharField(null=True, max_length=20)
+    jindo = models.CharField(null=True, max_length=20)
+
+    objects = models.DjongoManager()
