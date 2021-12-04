5a1d8a87d770d77be95c0402b8eb3e3c44a1c14f
kim-chae-yeon <58394861+kim-chae-yeon@users.noreply.github.com>
2021-12-04 16:36:27 +0900

    tag 데이터베이스 수정 및 django downgrade, model 수정

--- client/clients/models.py
+++ client/clients/models.py
@@ -1,15 +1,30 @@
 from djongo import models
 
-class Category_log(models.Model):
-    _id = models.ObjectIdField()
-    user_id = models.IntegerField(null=True)
-    add_date = models.DateTimeField(auto_now_add=True)
+class Category(models.Model):
     grade = models.CharField(null=True, max_length=20)
     subject = models.CharField(null=True, max_length=20)
     achievement = models.CharField(null=True, max_length=20)
     site = models.CharField(null=True, max_length=20)
-    jobdam = models.CharField(null=True, max_length=20)
-    pilgi = models.CharField(null=True, max_length=20)
-    jindo = models.CharField(null=True, max_length=20)
+    tag_jobdam = models.CharField(null=True, max_length=20)
+    tag_pilgi = models.CharField(null=True, max_length=20)
+    tag_jindo = models.CharField(null=True, max_length=20)
+    tag_achivement = models.CharField(null=True, max_length=20)
 
+    class Meta:
+        abstract = True
+
+class Category_log(models.Model):
+    _id = models.ObjectIdField()
+    user_id = models.IntegerField(null=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+    
+    category = models.ArrayField(
+        model_container=Category
+    )
+    
     objects = models.DjongoManager()
+
+class Recommendation_log(models.Model):
+    _id = models.ObjectIdField()
+    user_id = models.IntegerField(null=True)
+    
